14d505b25e58b8d66cc19439f549873d200c4c67
zengfanbin <lethe_higanbana@sina.com>
2020-06-21 18:32:49 +0800

    前端渲染

--- service/service-edu/src/main/java/com/summersky/guli/service/edu/service/impl/TeacherServiceImpl.java
+++ service/service-edu/src/main/java/com/summersky/guli/service/edu/service/impl/TeacherServiceImpl.java
@@ -3,9 +3,11 @@ package com.summersky.guli.service.edu.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.summersky.guli.common.base.result.R;
+import com.summersky.guli.service.edu.entity.Course;
 import com.summersky.guli.service.edu.entity.Teacher;
 import com.summersky.guli.service.edu.entity.vo.TeacherQueryVo;
 import com.summersky.guli.service.edu.feign.OssFileService;
+import com.summersky.guli.service.edu.mapper.CourseMapper;
 import com.summersky.guli.service.edu.mapper.TeacherMapper;
 import com.summersky.guli.service.edu.service.TeacherService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -13,6 +15,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +32,8 @@ public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> impl
 
     @Autowired
     private OssFileService ossFileService;
+    @Autowired
+    private CourseMapper courseMapper;
 
     @Override
     public Page<Teacher> selectPage(Page<Teacher> pageParam, TeacherQueryVo teacherQueryVo) {
@@ -88,5 +93,20 @@ public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> impl
         }
 
         return false;
+    }
+
+    @Override
+    public Map<String, Object> selectTeacherInfoById(String id) {
+        Teacher teacher = baseMapper.selectById(id);
+
+        QueryWrapper<Course> courseQueryWrapper = new QueryWrapper<>();
+        courseQueryWrapper.eq("teacher_id", id);
+        List<Course> courseList = courseMapper.selectList(courseQueryWrapper);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("teacher", teacher);
+        map.put("courseList", courseList);
+
+        return map;
     }
 }
